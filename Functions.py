74e6b53ef2ca8e3ac9e583832eb7f482f5a1f32d
BroPhilipp <philipphaizmann@googlemail.com>
2023-01-08 13:21:50 +0100

    work on c_pets_list function

--- Functions.py
+++ Functions.py
@@ -7,3 +7,15 @@ def open_file(file):
             content.append(line.strip())
     return content
         
+def c_pets_list(file):
+    content = open_file(file)
+    pets_list =[]
+    pos_empty_str = []
+    summary_text = []
+    for i,x in enumerate(content):
+        if x == "":
+            pos_empty_str.append(i)
+    summary_text.append(content[int(pos_empty_str[1]+1):int(pos_empty_str[2])])
+    for i in summary_text:
+        print(i)
+c_pets_list(file)
